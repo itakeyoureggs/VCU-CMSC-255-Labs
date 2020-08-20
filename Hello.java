bd04f959a4d38275838f6a756de9c9707358d58c
itakeyoureggs <69978544+itakeyoureggs@users.noreply.github.com>
2020-08-20 11:27:57 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,26 @@
+/************************************
+*Hello
+*************************************
+*Steven Singh
+*8/20/20
+*CMSC 255 001
+************************************/
+
+
+
+
+public class Hello {
+	
+	public static void main(String[] args) {
+	
+		System.out.println("Steven Singh");
+		System.out.println("Project Name: Hello");
+		System.out.println("Printing to screen details about myself");
+		System.out.println("Today is 8/20/20");
+		System.out.println("CMSC 255-001");
+		System.out.println();
+		
+		}
+		}
+		
+		
